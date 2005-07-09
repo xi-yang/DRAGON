cc914d2965df50a8828909b9ca51d86c4ed3f15c
chris <chris@625cd551-afe4-0310-9ad4-9d199310cf85>
2005-07-09 04:36:26 +0000

    Minor improvements to rc script.  Include narb config file path in the args, use 1 variable for etc dir instead of many hardcoded paths...
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1593 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/dragon.sh
+++ dragon-sw/dragon.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
 PREFIX=/usr/local/dragon
+ETC_DIR=$PREFIX/etc
 
 ZEBRA_DAEMON=$PREFIX/sbin/zebra
-ZEBRA_ARGS="-d -f /usr/local/dragon/etc/zebra.conf"
+ZEBRA_ARGS="-d -f $ETC_DIR/zebra.conf"
 
 OSPF_DAEMON=$PREFIX/sbin/ospfd
-OSPF_ARGS="-d -f /usr/local/dragon/etc/ospfd.conf"
+OSPF_ARGS="-d -f $ETC_DIR/ospfd.conf"
 
 RSVP_DAEMON=$PREFIX/bin/RSVPD
-RSVP_ARGS="-c /usr/local/dragon/etc/RSVPD.conf -d -o /var/log/RSVPD.log -L select"
+RSVP_ARGS="-c $ETC_DIR/RSVPD.conf -d -o /var/log/RSVPD.log -L select"
 
 DRAGON_DAEMON=$PREFIX/bin/dragon
-DRAGON_ARGS="-d -f /usr/local/dragon/etc/dragon.conf"
+DRAGON_ARGS="-d -f $ETC_DIR/dragon.conf"
 
 # for NARBs we need 2 ospfd instances.  the above
 # OSPF_DAEMON/OSPF_ARGS variables are not used for NARBs:
 
 OSPF_INTER_DAEMON=$PREFIX/sbin/ospfd
-OSPF_INTER_ARGS="-d -I -P 2604 -f /usr/local/dragon/etc/ospfd-inter.conf"
+OSPF_INTER_ARGS="-d -I -P 2604 -f $ETC_DIR/ospfd-inter.conf"
 
 OSPF_INTRA_DAEMON=$PREFIX/sbin/ospfd
-OSPF_INTRA_ARGS="-d -P 2614 -f /usr/local/dragon/etc/ospfd-intra.conf"
+OSPF_INTRA_ARGS="-d -P 2614 -f $ETC_DIR/ospfd-intra.conf"
 
 NARB_DAEMON=$PREFIX/sbin/narb
-NARB_ARGS="-d"
+NARB_ARGS="-d -f $ETC_DIR/narb.conf"
 
 #
 # need to have a way for 'stop' to recognize/kill both ospfd instances...
