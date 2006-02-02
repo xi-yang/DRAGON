c0231635668d959b7cebd362b1e3e8c3491e25ed
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2006-02-02 16:23:30 +0000

    Fixed a bug: wrong data type
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@227 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/zebra/ospfd/ospf_te.c
+++ dragon-sw/zebra/ospfd/ospf_te.c
@@ -539,10 +539,10 @@ set_linkparams_ifsw_cap1 (struct te_link_subtlv_link_ifswcap *para, u_char swcap
   {
   	 para->header.length = htons(base_length + sizeof(struct link_ifswcap_specific_tdm));
   }
-  //else if ( swcap == LINK_IFSWCAP_SUBTLV_SWCAP_L2SC)
-  //{
-  //	 para->header.length = htons(base_length + sizeof(struct link_ifswcap_specific_vlan));
-  //}
+  else if ( swcap == LINK_IFSWCAP_SUBTLV_SWCAP_L2SC)
+  {
+  	 para->header.length = htons(base_length + sizeof(struct link_ifswcap_specific_vlan));
+  }
   else
   	 para->header.length = htons(base_length);
    para->link_ifswcap_data.switching_cap = swcap;
@@ -1232,7 +1232,7 @@ show_vty_link_subtlv_ifsw_cap_network (struct vty *vty, struct te_tlv_header *tl
   }
   else if (strncmp(swcap, "l2sc", 4) == 0)
   {
-	  if (vty != NULL && (*(u_int32_t*)v == ntohs(MAX_VLAN_NUM/8+3) && *(v+2) ==IFSWCAP_SPECIFIC_VLAN_VERSION)) {
+	  if (vty != NULL && (*(u_int16_t*)v == ntohs(MAX_VLAN_NUM/8+3) && *(v+2) ==IFSWCAP_SPECIFIC_VLAN_VERSION)) {
 	    v += 3;
 	    vty_out (vty, "  -- L2SC specific information-- : Available VLAN tag set:");
 	    for (i = 0; i < MAX_VLAN_NUM; i++)
