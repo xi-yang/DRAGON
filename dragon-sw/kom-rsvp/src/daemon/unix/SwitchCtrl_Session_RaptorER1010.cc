344a9903ea25b61499cddb9315a0befaf91532a7
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2006-02-24 21:13:35 +0000

    Addition of Raptor (vendor) ER1010 (model) Control Module. Commit after compilation -- w/o testing.
    In addition, changes made in SNMP_Session.cc
    > rfc2674_compatible logic enforced.
    > added port -- portbits conversion 
    > changed removePort... logic
    Regression testings required.
    
    
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@854 625cd551-afe4-0310-9ad4-9d199310cf85

---
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_RaptorER1010.cc
@@ -0,0 +1,215 @@
+/****************************************************************************
+
+Raptor (vendor) ER1010 (model) Control Module source file SwitchCtrl_Session_RatporER1010.cc
+Created by Xi Yang @ 02/24/2006
+To be incorporated into KOM-RSVP-TE package
+
+****************************************************************************/
+
+#include "SwitchCtrl_Session_RaptorER1010.h"
+#include "RSVP_Log.h"
+
+bool SwitchCtrl_Session_RaptorER1010::movePortToVLANAsUntagged(uint32 port, uint32 vlanID)
+{
+    bool ret = true;
+    vlanPortMap * vpmAll = NULL, *vpmUntagged = NULL;
+
+    if ((!active) || port==SWITCH_CTRL_PORT || vlanID<MIN_VLAN || vlanID>MAX_VLAN) 
+    	return false; //don't touch the control port!
+    int old_vlan = getVLANbyUntaggedPort(port);
+
+    //@@@@ Raptor allows a port to be tagged in two VLANs
+    //@@@@ Do we still need to do the following removal?
+    //@@@@ Code haven't been changed after copied from SNMP_Session.cc
+    /*
+    if (old_vlan) { //Remove untagged port from old VLAN
+        mask=(~(1<<(32-port))) & 0xFFFFFFFF;
+        vpmUntagged = getVlanPortMapById(vlanPortMapListUntagged, old_vlan);
+        if (vpmUntagged)
+            vpmUntagged->ports&=mask;
+        vpmAll = getVlanPortMapById(vlanPortMapListAll, old_vlan);
+        if (vpmAll)
+    	    vpmAll->ports&=mask;
+        //Set original ports back to their "tagged" or "untagged" states
+        if (vpmUntagged) ret&=setVLANPortTag(vpmUntagged->ports , old_vlan); 
+        //remove THIS untagged port out of the old VLAN
+        if (vpmUntagged) ret&=setVLANPort(vpmUntagged->ports, old_vlan); 
+   }
+    */
+
+    vpmUntagged = getVlanPortMapById(vlanPortMapListUntagged, vlanID);
+    if (vpmUntagged)
+        SetPortBit(vpmUntagged->portbits, port);
+    vpmAll = getVlanPortMapById(vlanPortMapListAll, vlanID);
+    if (vpmAll) {
+        SetPortBit(vpmAll->portbits, port);
+        ret&=setVLANPort(vpmAll->portbits, RAPTOR_VLAN_BITLEN, vlanID) ;
+    }
+
+    //@@@@ Does Raptor require set PVID to the VLAN#?
+    //ret&=setVLANPVID(port, vlanID); //Set pvid
+
+    return ret;
+}
+
+bool SwitchCtrl_Session_RaptorER1010::movePortToVLANAsTagged(uint32 port, uint32 vlanID)
+{
+    bool ret = true;
+    vlanPortMap * vpmAll = NULL, *vpmUntagged = NULL;
+
+    if ((!active) || port==SWITCH_CTRL_PORT || vlanID<MIN_VLAN || vlanID>MAX_VLAN) 
+    	return false; //don't touch the control port!
+
+    //there is no need to remove a to-be-tagged-in-new-VLAN port from old VLAN
+    vpmAll = getVlanPortMapById(vlanPortMapListAll, vlanID);
+    if (vpmAll) {
+        SetPortBit(vpmAll->portbits, port);
+        ret&=setVLANPort(vpmAll->portbits, RAPTOR_VLAN_BITLEN, vlanID) ;
+    } else
+        return false;
+
+    vpmUntagged = getVlanPortMapById(vlanPortMapListUntagged, vlanID);
+    if (vpmUntagged)
+        ret&=setVLANPortTag(vpmUntagged->portbits, RAPTOR_VLAN_BITLEN, vlanID);
+
+    return ret;
+}
+
+
+bool SwitchCtrl_Session_RaptorER1010::removePortFromVLAN(uint32 port, uint32 vlanID)
+{
+    bool ret = true;
+    vlanPortMap * vpmAll = NULL, *vpmUntagged = NULL;
+
+    if ((!active) || !rfc2674_compatible || port==SWITCH_CTRL_PORT)
+    	return false; //don't touch the control port!
+
+    if (vlanID == 0)
+        vlanID = getVLANbyUntaggedPort(port);
+    if (vlanID>=MIN_VLAN && vlanID<=MAX_VLAN) {
+    	 vpmAll = getVlanPortMapById(vlanPortMapListAll, vlanID);
+        if (vpmAll) {
+            ResetPortBit(vpmAll->portbits, port);
+            vpmUntagged = getVlanPortMapById(vlanPortMapListUntagged, vlanID);
+            if (vpmUntagged) {
+                ResetPortBit(vpmUntagged->portbits, port);
+                //@@@@    ?+?   Set pvid to default vlan ID;
+                //ret&=setVLANPVID(port, 1); 
+                ret&=setVLANPortTag(vpmUntagged->portbits, RAPTOR_VLAN_BITLEN, vlanID);
+            }
+            ret &= setVLANPort(vpmAll->portbits, RAPTOR_VLAN_BITLEN, vlanID);
+        }
+    }
+
+    return ret;
+}
+
+
+/////////--------Raptor ER1010 specific functions------///////////
+
+
+///////////------Raptor ER1010 specific hooks -------///////////
+
+bool SwitchCtrl_Session_RaptorER1010::hook_createVLAN(const uint32 vlanID)
+{
+    struct snmp_pdu *pdu;
+    struct snmp_pdu *response;
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len = MAX_OID_LEN;
+    char type, value[128], oid_str[128];
+    int status;
+    String tag_oid_str = ".1.3.6.1.2.1.17.7.1.4.3.1.5";
+
+    if (!active) //not initialized or session has been disconnected
+        return false;
+    // Create the PDU for the data for our request. 
+    pdu = snmp_pdu_create(SNMP_MSG_SET);
+
+    // vlan port list 
+    sprintf(oid_str, "%s.%d", tag_oid_str.chars(), vlanID);
+    status = read_objid(oid_str, anOID, &anOID_len);
+    type='i'; 
+    strcpy(value, "4");
+
+    status = snmp_add_var(pdu, anOID, anOID_len, type, value);
+
+    // Send the Request out. 
+    status = snmp_synch_response(snmpSessionHandle, pdu, &response);
+
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+    	snmp_free_pdu(response);
+    }
+    else {
+        if (status == STAT_SUCCESS){
+        LOG(4)( Log::MPLS, "VLSR: SNMP: Setting VLAN PVID", switchInetAddr, "failed. Reason : ", snmp_errstring(response->errstat));
+        }
+        else
+      	    snmp_sess_perror("snmpset", snmpSessionHandle);
+        if(response) snmp_free_pdu(response);
+        return false;
+    }
+    return true;
+}
+
+bool SwitchCtrl_Session_RaptorER1010::hook_removeVLAN(const uint32 vlanID)
+{
+    struct snmp_pdu *pdu;
+    struct snmp_pdu *response;
+    oid anOID[MAX_OID_LEN];
+    size_t anOID_len = MAX_OID_LEN;
+    char type, value[128], oid_str[128];
+    int status;
+    String tag_oid_str = ".1.3.6.1.2.1.17.7.1.4.3.1.5";
+
+    if (!active) //not initialized or session has been disconnected
+        return false;
+    // Create the PDU for the data for our request. 
+    pdu = snmp_pdu_create(SNMP_MSG_SET);
+
+    // vlan port list 
+    sprintf(oid_str, "%s.%d", tag_oid_str.chars(), vlanID);
+    status = read_objid(oid_str, anOID, &anOID_len);
+    type='i'; 
+    strcpy(value, "6");
+
+    status = snmp_add_var(pdu, anOID, anOID_len, type, value);
+
+    // Send the Request out. 
+    status = snmp_synch_response(snmpSessionHandle, pdu, &response);
+
+    if (status == STAT_SUCCESS && response->errstat == SNMP_ERR_NOERROR) {
+    	snmp_free_pdu(response);
+    }
+    else {
+        if (status == STAT_SUCCESS){
+        LOG(4)( Log::MPLS, "VLSR: SNMP: Setting VLAN PVID", switchInetAddr, "failed. Reason : ", snmp_errstring(response->errstat));
+        }
+        else
+      	    snmp_sess_perror("snmpset", snmpSessionHandle);
+        if(response) snmp_free_pdu(response);
+        return false;
+    }
+    return true;
+}
+
+bool SwitchCtrl_Session_RaptorER1010::hook_isVLANEmpty(const vlanPortMap &vpm)
+{
+    return (memcmp(vpm.portbits, "\0\0\0\0\0\0", 6) == 0);
+}
+
+void SwitchCtrl_Session_RaptorER1010::hook_getPortMapFromSnmpVars(vlanPortMap &vpm, netsnmp_variable_list *vars)
+{
+    memset(&vpm, 0, sizeof(vlanPortMap));
+    if (vars->val.bitstring ){
+        for (int i = 0; i < vars->val_len && i < RAPTOR_VLAN_BITLEN/8; i++) {
+            vpm.portbits[i] = vars->val.bitstring[i];
+       }
+    }
+}
+
+bool SwitchCtrl_Session_RaptorER1010::hook_hasPortinVlanPortMap(vlanPortMap &vpm, uint32  port)
+{
+    return HasPortBit(vpm.portbits, port);
+}
+
+
---
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_RaptorER1010.h
@@ -0,0 +1,46 @@
+/****************************************************************************
+
+Raptor (vendor) ER1010 (model) Control Module header file SwitchCtrl_Session_RatporER1010.h
+Created by Xi Yang @ 02/24/2006
+To be incorporated into KOM-RSVP-TE package
+
+****************************************************************************/
+
+#ifndef _SWITCHCTRL_SESSION_RAPTORER1010_H_
+#define _SWITCHCTRL_SESSION_RAPTORER1010_H_
+
+#include "SNMP_Session.h"
+
+class SwitchCtrl_Session_RaptorER1010: public SNMP_Session
+{
+	
+public:
+	SwitchCtrl_Session_RaptorER1010(): SNMP_Session() { rfc2674_compatible = snmp_enabled = true;}
+	SwitchCtrl_Session_RaptorER1010(const RSVP_String& sName, const NetAddress& swAddr): SNMP_Session(sName, swAddr) 
+		{ rfc2674_compatible = snmp_enabled = true; }
+	virtual ~SwitchCtrl_Session_RaptorER1010() { }
+
+	////////// Below are vendor specific functions/////////
+	virtual bool movePortToVLANAsTagged(uint32 port, uint32 vlanID);
+	virtual bool movePortToVLANAsUntagged(uint32 port, uint32 vlanID);
+	virtual bool removePortFromVLAN(uint32 port, uint32 vlanID);
+
+	///////////------QoS Functions ------/////////
+	//virtual bool policeInputBandwidth(bool do_undo, uint32 input_port, uint32 vlan_id, float committed_rate, int burst_size=0, float peak_rate=0.0,  int peak_burst_size=0);
+	//virtual bool limitOutputBandwidth(bool do_undo,  uint32 output_port, uint32 vlan_id, float committed_rate, int burst_size=0, float peak_rate=0.0,  int peak_burst_size=0);
+
+	////////-----Vendor/Model specific hook functions------//////
+	virtual bool hook_createVLAN(const uint32 vlanID);
+	virtual bool hook_removeVLAN(const uint32 vlanID);
+	virtual bool hook_isVLANEmpty(const vlanPortMap &vpm);
+       virtual void hook_getPortMapFromSnmpVars(vlanPortMap &vpm, netsnmp_variable_list *vars);
+	virtual bool hook_hasPortinVlanPortMap(vlanPortMap &vpm, uint32  port);
+
+	///////------Vendor/model specific functions--------///////
+
+};
+
+#define RAPTOR_VLAN_BITLEN		48
+
+#endif //ifndef _SwitchCtrl_Session_RaptorER1010_H_
+
