b23fbf836004c491c9b0d01bf47b0da0df790e61
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2006-03-28 21:46:40 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@260 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/zebra/ospfd/ospf_te_lsa.c
+++ dragon-sw/zebra/ospfd/ospf_te_lsa.c
@@ -1779,14 +1779,14 @@ ospf_te_area_lsa_uni_refresh (struct ospf_interface *oi)
   }
 
   if (oi->uni_data->te_lsa_link)
-      if (! ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_link))
+      if (ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_link) != 0)
       {
           zlog_info ("ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_link) on %s failed.", oi->ifp->name);
           goto out;
       }	
 
   if (oi->uni_data->te_lsa_rtid)
-      if (! ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_rtid))
+      if (! ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_rtid)  != 0)
       {
           zlog_info ("ospf_te_area_lsa_uni_refresh1(oi, oi->uni_data->te_lsa_rtid) on %s failed.", oi->ifp->name);
           goto out;
