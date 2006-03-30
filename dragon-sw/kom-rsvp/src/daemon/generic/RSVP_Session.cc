ac32f4ce64ffc76350c4737d6b69fda801e7a78d
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2006-03-30 16:04:55 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@928 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/generic/RSVP_Session.cc
+++ dragon-sw/kom-rsvp/src/daemon/generic/RSVP_Session.cc
@@ -445,8 +445,8 @@ void Session::processPATH( const Message& msg, Hop& hop, uint8 TTL ) {
 */
 	bool fromLocalAPI = (&hop.getLogicalInterface() == RSVP_Global::rsvp->getApiLif()
 		&& (msg.getRSVP_HOP_Object().getAddress() == LogicalInterface::loopbackAddress
-		|| msg.getRSVP_HOP_Object().getAddress() == loopback)
-		|| RSVP_Global::rsvp->findInterfaceByAddress(msg.getRSVP_HOP_Object().getAddress()));
+		|| msg.getRSVP_HOP_Object().getAddress() == loopback
+		|| RSVP_Global::rsvp->findInterfaceByAddress(msg.getRSVP_HOP_Object().getAddress())));
 
 	bool fromUniClient = (&hop.getLogicalInterface() == RSVP_Global::rsvp->getApiLif()
 						&&((Message*)&msg)->getDRAGON_UNI_Object() != NULL);
