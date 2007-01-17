e9f5e955f36b47a63525cee0a605a921337a9e8c
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2007-01-17 23:27:46 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1170 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/RSVP_MPLS.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/RSVP_MPLS.cc
@@ -265,7 +265,7 @@ bool MPLS::bindInAndOut( PSB& psb, const MPLS_InLabel& il, const MPLS_OutLabel& 
 				if ( (*sessionIter)->getSessionName().leftequal("subnet-uni") ) {
 					noError = true;
 					continue;
- 				} else if ( (*sessionIter)->getSubnetUniSrc() || (*sessionIter)->getSubnetUniDest() ) {
+ 				} else if ( ((*iter).inPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_SRC || ((*iter).inPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_DEST ) {
 					noError = true;
 					continue;
 				}
@@ -527,10 +527,10 @@ void MPLS::deleteInLabel(PSB& psb, const MPLS_InLabel* il ) {
 				 //@@@@ >>Xi2007<<
 				if ( (*sessionIter)->getSessionName().leftequal("subnet-uni") ) {
 					continue;
-				} else if ((*sessionIter)->getSubnetUniSrc()) {//@@@@ >>Xi2007<<
+				} else if ( ((*iter).inPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_SRC )
 					(*sessionIter)->getSubnetUniSrc()->releaseRsvpPath();
 					continue;
-				} else if ((*sessionIter)->getSubnetUniDest()) {//@@@@ >>Xi2007<<
+				} else if ( ((*iter).inPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_DEST )
 					continue;
 				}
 				
