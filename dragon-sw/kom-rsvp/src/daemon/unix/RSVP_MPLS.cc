4c8616f9efcb80b45a5a633ffa407ac02fadff03
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2007-04-11 19:24:58 +0000

    turning off per-session timeslots sync.
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1394 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/RSVP_MPLS.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/RSVP_MPLS.cc
@@ -296,10 +296,10 @@ bool MPLS::bindInAndOut( PSB& psb, const MPLS_InLabel& il, const MPLS_OutLabel& 
 
                                                 if ( ((*iter).inPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_SRC ||((*iter).outPort >> 16) == LOCAL_ID_TYPE_SUBNET_UNI_DEST ) {
                                                     // $$$$ temp testing code ///
-                                                    if ( !((SwitchCtrl_Session_SubnetUNI*)(*sessionIter))->syncTimeslotsMap() ) {
-                                                        (*sessionIter)->disconnectSwitch();
-                                                        return false;
-                                                    }
+                                                    //if ( !((SwitchCtrl_Session_SubnetUNI*)(*sessionIter))->syncTimeslotsMap() ) {
+                                                    //    (*sessionIter)->disconnectSwitch();
+                                                    //    return false;
+                                                    //}
                                                     
                                                     //create VCG for LOCAL_ID_TYPE_SUBNET_UNI_SRC OR LOCAL_ID_TYPE_SUBNET_UNI_DEST
                                                     if ( !((SwitchCtrl_Session_SubnetUNI*)(*sessionIter))->createVCG((*iter).vlanTag) ) {
