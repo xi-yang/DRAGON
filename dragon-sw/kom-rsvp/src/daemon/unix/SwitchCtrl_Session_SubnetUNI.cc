1c9457a072b8912e9db53ed0b2631239281142f0
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2007-09-15 01:44:48 +0000

    handling TRUNCATED TL1 output
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1544 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_SubnetUNI.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_SubnetUNI.cc
@@ -1053,12 +1053,11 @@ bool SwitchCtrl_Session_SubnetUNI::hasVCG_TL1(String& vcgName)
 
     sprintf(strCOMPLD, "M  %d COMPLD", getCurrentCtag());
     sprintf(strDENY, "M  %d DENY", getCurrentCtag());
-    //ret = readShell(strCOMPLD, strDENY, 1, 5);
-    ret = ReadShellPattern(bufCmd, strCOMPLD, strDENY, ";", "TRUNCATED", 5);
+    ret = readShell(strCOMPLD, strDENY, 1, 5);
     if (ret == 1 || ret == READ_STOP) 
     {
         LOG(3)(Log::MPLS, vcgName, " VCG does exist.\n", bufCmd);
-        readShell(SWITCH_PROMPT, NULL, 1, 5);
+        readShell(SWITCH_PROMPT, "TRUNCATED\"", 1, 5);
         return true;
     }
     else if (ret == 2)
