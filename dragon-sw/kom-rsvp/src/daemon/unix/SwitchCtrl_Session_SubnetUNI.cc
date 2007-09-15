17d55894214dae4c8a1df32753e55d01eaae1b93
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2007-09-15 02:55:22 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1547 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_SubnetUNI.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_SubnetUNI.cc
@@ -1048,7 +1048,7 @@ bool SwitchCtrl_Session_SubnetUNI::hasVCG_TL1(String& vcgName)
 {
     int ret = 0;
 
-    sprintf( bufCmd, "rtrv-vcg::%s:%d;\n", vcgName.chars(), getNewCtag() );
+    sprintf( bufCmd, "rtrv-vcg::%s:%d;", vcgName.chars(), getNewCtag() );
     if ( (ret = writeShell(bufCmd, 5)) < 0 ) goto _out;
 
     sprintf(strCOMPLD, "M  %d COMPLD", getCurrentCtag());
@@ -1057,8 +1057,7 @@ bool SwitchCtrl_Session_SubnetUNI::hasVCG_TL1(String& vcgName)
     if (ret == 1 || ret == READ_STOP) 
     {
         LOG(3)(Log::MPLS, vcgName, " VCG does exist.\n", bufCmd);
-        //readShell(SWITCH_PROMPT, "TRUNCATED\"", true, 1, 5);
-        readShell(SWITCH_PROMPT, NULL, 1, 5);
+        readShell(SWITCH_PROMPT, "TRUNCATED\"", true, 1, 5);
         return true;
     }
     else if (ret == 2)
@@ -1599,7 +1598,7 @@ bool SwitchCtrl_Session_SubnetUNI::syncTimeslotsMapVCG_TL1(uint8 *ts_bitmask, ui
 
     getCienaLogicalPortString(OMPortString, ETTPString, logicalPort);
 
-    sprintf(bufCmd, "rtrv-vcg::all:%d;\n", getCurrentCtag());
+    sprintf(bufCmd, "rtrv-vcg::all:%d;", getCurrentCtag());
     if ( (ret = writeShell(bufCmd, 5)) < 0 ) goto _out;
 
     sprintf(strCOMPLD, "M  %d COMPLD", getCurrentCtag());
@@ -1615,7 +1614,7 @@ bool SwitchCtrl_Session_SubnetUNI::syncTimeslotsMapVCG_TL1(uint8 *ts_bitmask, ui
         }
         else if (ret == 2)
         {
-            while ((ret = ReadShellPattern(bufCmd, (char*)OMPortString.chars(), "GROUPMEM=", "VCGFAILUREBASESEV=",  ";", 5)) != READ_STOP)
+            while ((ret = ReadShellPattern(bufCmd, (char*)OMPortString.chars(), "GROUPMEM=", "MEMBERDETAIL=",  ";", 5)) != READ_STOP)
             { // if (ret == 3), we have reach the end, i.e., ";"...
                 if (ret == 1)
                 {
