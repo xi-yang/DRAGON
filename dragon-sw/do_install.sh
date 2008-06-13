b7986c091918a74281bdd450a9f539573adc62df
chris <chris@625cd551-afe4-0310-9ad4-9d199310cf85>
2008-06-13 15:51:08 +0000

    Update do_install.sh to recognize Darwin systems.
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@1850 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/do_install.sh
+++ dragon-sw/do_install.sh
@@ -81,6 +81,36 @@ case `uname` in
     echo '' && \
      echo "dragon-sw install finished."
     ;;
+  *Darwin)
+    echo This is Darwin
+
+    echo '' && \
+        echo "installing kom-rsvp..."
+    cd kom-rsvp
+    gmake install
+    if test $? != 0; then
+	echo "dragon-sw: kom-rsvp gmake install error!"
+	exit 1
+    fi
+
+    echo '' && \
+        echo "installing zebra..."
+    cd ../zebra
+    make install
+    if test $? != 0; then
+	echo "dragon-sw: zebra make install error!"
+	exit 1
+    fi
+
+    echo '' && \
+     echo "dragon-sw install finished."
+
+    echo '' && \
+        echo "if log file rotation is desired, please do the following..." && \
+        echo "  - install logrotate via darwinports (www.darwinports.com)" && \
+        echo "  - cron script: utils/logrotate4dragon (run hourly)" && \
+        echo "  - configure logrotate to use utils/logrotate4dragon.conf"
+    ;;
   *)
     echo Do not know what kind of system this is, do it by hand
     ;;
