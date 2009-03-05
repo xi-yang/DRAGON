2ca06c92d672fe6e0c268dde56830a491a11052d
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-03-05 19:21:28 +0000

    Update version numbers for new snapshot
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2126 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/zebra/dragon/dragon_version.c
+++ dragon-sw/zebra/dragon/dragon_version.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*** To automate version string generation from SVN, CHANGE HERE into ANYTHING before each 'commit' --> "02/24/2009" ***/
+/*** To automate version string generation from SVN, CHANGE HERE into ANYTHING before each 'commit' --> "03/05/2009" ***/
 
 const char* dragon_version_string() {
     static char dvString[100];
