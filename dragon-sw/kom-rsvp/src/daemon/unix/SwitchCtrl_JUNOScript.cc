6fc1ff629739edfe2638459c33e1e5c33bbd475a
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-03-18 20:55:22 +0000

    JUNOScriptComposer and JUNOScriptParser classes
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2166 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_JUNOScript.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_JUNOScript.cc
@@ -7,41 +7,49 @@ To be incorporated into KOM-RSVP-TE package
 ****************************************************************************/
 
 #include "SwitchCtrl_JUNOScript.h"
-/*
-static char *jsCreateVlan = "<rpc>\
+#include "RSVP_Log.h"
+
+//	set interfaces ge-0/0/0 unit 0 family ethernet-switching vlan members dynamic_vlan_300
+
+const char *JUNOScriptMovePortVlanComposer::jsTemplate = "<rpc>\
+    <load-configuration>\
+      <configuration>\
+         <interfaces>\
+            <interface>\
+		<name>ge-0/0/1</name>\
+		<unit><name>0<name>\
+		  <family>\
+		      <ethernet-switching>\
+			    <port-mode>access</port-mode>\
+			    <vlan><members>dynamic_vlan_300</members></vlan>\
+		      </ethernet-switching>\
+		  </family>\
+		</unit>\
+            </interface>\
+         </interfaces>\
+         <vlans>\
+            <vlan>\
+		<name>dyanmic_vlan_300</name>\
+		<interface>ge-0/0/1.0</interface>\
+            </vlan>\
+         </vlans>\
+       </configuration>\
+    </load-configuration>\
+</rpc>";
+
+const char *JUNOScriptVlanComposer::jsTemplate = "<rpc>\
     <load-configuration>\
       <configuration>\
          <vlans>\
             <vlan>\
-		<name>dynamic_vlan_XXXX</name>\
-		<description>DRAGON VLSR Created Dynamic VLAN (VID=XXXX)</description>\
-		<vlan-id>300</vlan-id>\
+		<name>dyanmic_vlan_300</name>\
             </vlan>\
          </vlans>\
        </configuration>\
     </load-configuration>\
 </rpc>";
 
-static char * jsDeleteVlan = "<rpc>\
-    <load-configuration>\
-      <configuration>\
-         <vlans>\
-            <vlan delete=\"delete\">\
-		<name>dynamic_vlan_XXXX</name>\
-            </vlan>\
-         </vlans>\
-       </configuration>\
-    </load-configuration>\
-</rpc>";
-*/
-
-#define XML_LOOP_NODESET(S, N) \
-    if (S != NULL && S->nodeNr > 0)  \
-        for (N=S->nodeTab[0]; N != NULL; N=N->next)
-
-
-//////////////////////////////////////////////////////////////////////////////
-
+////////////////////////////////////////////////////////////////////////////
 
 JUNOScriptParser::~JUNOScriptParser() 
 { 
@@ -219,4 +227,219 @@ bool JUNOScriptUnlockReplyParser::isSuccessful()
     return false;
 }
 
+///////////////////////////////////////////////////////////////////////
 
+
+JUNOScriptComposer::~JUNOScriptComposer()
+{
+    freeScriptDoc();
+    if (isScriptBufInternal && xmlScript != NULL)
+        delete xmlScript;
+}
+
+bool JUNOScriptComposer::initScriptDoc(const char* xmlBuf)
+{
+    freeScriptDoc();
+    xmlDoc = xmlReadMemory((const char*)xmlScript, strlen((const char*)xmlScript), (const char*)"junoscript.xml", NULL, 0);
+    if (xmlDoc == NULL) {
+        //$$$$ Log::
+        return false;
+    }
+    xpathCtx =  xmlXPathNewContext(xmlDoc);
+    if(xpathCtx == NULL) {
+        //$$$$ Log::
+        return false;
+    }
+    return true;
+}
+
+bool JUNOScriptComposer::makeScript()
+{
+    assert(xmlDoc && xmlScript);
+
+    xmlChar* xmlScriptNew = NULL;
+    int scriptLenNew = 0;
+    xmlDocDumpFormatMemory(xmlDoc, &xmlScriptNew, &scriptLenNew, 1);
+    if (xmlScriptNew == NULL && scriptLenNew <= 0)
+        return false;
+    if (isScriptBufInternal)
+    {
+        if (xmlScript != NULL)
+            xmlFree(xmlScript);
+        xmlScript = xmlScriptNew;
+        scriptLen = scriptLenNew;
+    }
+    else
+    {
+        if (scriptLenNew > scriptLen)
+        {
+            //$$$$ LOG:: external buffer too small
+            scriptLenNew = scriptLen;
+        }
+        memcpy((char*)xmlScript, (char*)xmlScriptNew, scriptLenNew);
+    }
+    return true;
+}
+
+bool JUNOScriptComposer::finishScript()
+{
+    bool ret = makeScript();
+    freeScriptDoc();
+    return ret;
+}
+
+void JUNOScriptComposer::freeScriptDoc()
+{
+    if (xpathCtx != NULL)
+    {
+        xmlXPathFreeContext(xpathCtx);
+        xpathCtx = NULL;
+    }
+    if (xmlDoc != NULL)  
+    {
+        xmlFreeDoc(xmlDoc);
+        xmlDoc = NULL;
+    }
+}
+
+/*
+tagged port: set interfaces ge-0/0/0 unit 0 family ethernet-switching port-mode trunk
+	set interfaces ge-0/0/0 unit 0 family ethernet-switching vlan members dynamic_vlan100
+	set vlans vlan100 interface ge-0/0/0.0     
+	//set interfaces ge-0/0/0 unit 0 family ethernet-switching native-vlan-id 100 (?)
+
+untagged port: set interfaces ge-0/0/0 unit 0 family ethernet-switching port-mode access
+	set vlans vlan100 interface ge-0/0/0.0
+	set interfaces ge-0/0/0 unit 0 family ethernet-switching native-vlan-id 100
+*/
+
+
+bool JUNOScriptMovePortVlanComposer::setPortAndVlan(uint32 portId, uint32 vlanId, bool isTrunking, bool isToDelete)
+{
+    if (xmlDoc == NULL)
+        if (!initScriptDoc(JUNOScriptMovePortVlanComposer::jsTemplate))
+        {
+            LOG(5)(Log::MPLS, "Error: initScriptDoc(JUNOScriptMovePortVlanComposer::jsTemplate) failed in JUNOScriptMovePortVlanComposer::setPortAndVlan(", portId, ", ", vlanId, ").");
+            freeScriptDoc();
+            return false;
+        }
+
+    char portName[32], vlanName[32];
+    sprintf(portName, "ge-%d/%d/%d", (portId & 0xf000)>>12, (portId & 0x0f00)>>8, portId&0xff);
+    sprintf(vlanName, "dynamic_vlan_%d", vlanId);
+
+    xmlXPathObjectPtr xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/interfaces/name", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)portName);
+    //do not delete this node
+
+    xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/interfaces/family/ethernet-switching/vlan/members", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)vlanName);
+    if (isToDelete)
+        xmlNewProp(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"delete", (xmlChar*)"delete");
+
+    xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/interfaces/family/ethernet-switching/port-mode", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    if (isTrunking)
+    {
+        xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"trunk");
+        if (isToDelete)
+            xmlNewProp(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"delete", (xmlChar*)"delete");
+         //PVID not set for trunk-mode port
+    }
+    else
+    {
+        xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"access");
+        if (isToDelete)
+            xmlNewProp(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"delete", (xmlChar*)"delete");
+        xmlNodePtr node1 = xmlNewNode(NULL, (xmlChar*)"native-vlan-id");
+        xmlNodeSetContent(node1, (xmlChar*)vlanName);
+        if (isToDelete)
+            xmlNewProp(node1, (xmlChar*)"delete", (xmlChar*)"delete");
+        xmlAddNextSibling(xpathObj->nodesetval->nodeTab[0], node1);  // create //rpc/load-configuration/configuration/interfaces/family/ethernet-switching/native-vlan-id
+    }
+    
+    xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/vlans/vlan/name", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)vlanName);
+    //do not delete this node
+
+    xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/vlans/vlan/interface", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    strcat(portName, ".0");
+    xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)portName);
+    if (isToDelete)
+        xmlNewProp(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"delete", (xmlChar*)"delete");
+
+    if (xpathObj) xmlXPathFreeObject(xpathObj);
+    return finishScript();
+}
+
+
+bool JUNOScriptVlanComposer::setVlan(uint32 vlanId, bool isToDelete)
+{
+    if (xmlDoc == NULL)
+        if (!initScriptDoc(JUNOScriptVlanComposer::jsTemplate))
+        {
+            LOG(3)(Log::MPLS, "Error: initScriptDoc(JUNOScriptVlanComposer::jsTemplate) failed in JUNOScriptVlanComposer::setVlan(", vlanId, ").");
+            freeScriptDoc();
+            return false;
+        }
+
+    char vlanName[32];
+    sprintf(vlanName, "dynamic_vlan_%d", vlanId);
+
+    xmlXPathObjectPtr xpathObj = xmlXPathEvalExpression((xmlChar*)"//rpc/load-configuration/configuration/vlans/vlan/name", xpathCtx);
+    if (xpathObj == NULL || xpathObj->nodesetval == NULL ||xpathObj->nodesetval->nodeNr == 0)
+    {
+        if (xpathObj) xmlXPathFreeObject(xpathObj);
+        freeScriptDoc();
+        return false;
+    }  
+    xmlNodeSetContent(xpathObj->nodesetval->nodeTab[0], (xmlChar*)vlanName);
+    if (isToDelete)
+    {
+        xmlNewProp(xpathObj->nodesetval->nodeTab[0], (xmlChar*)"delete", (xmlChar*)"delete");
+    }
+    else 
+    {
+        char vlanString[64];
+        sprintf(vlanString, "%d", vlanId);
+        xmlNodePtr node1 = xmlNewNode(NULL, (xmlChar*)"vlan-id");
+        xmlNodeSetContent(node1, (xmlChar*)vlanString);
+        xmlAddNextSibling(xpathObj->nodesetval->nodeTab[0], node1);
+        sprintf(vlanString, "DRAGON VLSR Created Dynamic VLAN (VID=%d)", vlanId);
+        node1 = xmlNewNode(NULL, (xmlChar*)"description");
+        xmlNodeSetContent(node1, (xmlChar*)vlanString);
+        xmlAddNextSibling(xpathObj->nodesetval->nodeTab[0], node1);
+    }
+
+    if (xpathObj) xmlXPathFreeObject(xpathObj);
+    return finishScript();
+}
--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_JUNOScript.h
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_JUNOScript.h
@@ -10,13 +10,21 @@ To be incorporated into KOM-RSVP-TE package
 
 using namespace std;
 
+#include "RSVP_System.h"
 #include "RSVP_String.h"
-
 #include <libxml/xmlmemory.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <libxml/xpath.h>
 #include <libxml/xpathInternals.h>
+
+
+
+#define XML_LOOP_NODESET(S, N) \
+    if (S != NULL && S->nodeNr > 0)  \
+        for (N=S->nodeTab[0]; N != NULL; N=N->next)
+
+/////////////////////////////////////////////////////////////////
 
 class JUNOScriptParser
 {
@@ -65,5 +73,52 @@ public:
     bool isSuccessful();
 };
 
+///////////////////////////////////////////////////////////
+
+class JUNOScriptComposer
+{
+public:
+   JUNOScriptComposer(): xmlScript(NULL), xmlDoc(NULL), xpathCtx(NULL), isScriptBufInternal(true) { }
+   JUNOScriptComposer(char* buf, int len): xmlScript((xmlChar*)buf), scriptLen(len), xmlDoc(NULL), xpathCtx(NULL) , isScriptBufInternal(false) { }
+    virtual ~JUNOScriptComposer();
+    bool initScriptDoc(const char* xmlBuf);
+    void freeScriptDoc();
+    bool makeScript();
+    bool finishScript();
+    char* getScript() { return (char*)xmlScript; }
+
+protected:
+    xmlChar* xmlScript;
+    int scriptLen;
+    xmlDocPtr xmlDoc;
+    xmlXPathContextPtr xpathCtx;
+    bool isScriptBufInternal;
+
+private:
+
+};
+
+class JUNOScriptMovePortVlanComposer: public JUNOScriptComposer
+{
+public:
+    JUNOScriptMovePortVlanComposer() {}
+    virtual ~JUNOScriptMovePortVlanComposer() {}
+    bool setPortAndVlan(uint32 portId, uint32 vlanId, bool isTrunking, bool isToDelete);
+
+private:
+    static const char* jsTemplate;
+};
+
+class JUNOScriptVlanComposer: public JUNOScriptComposer
+{
+public:
+    JUNOScriptVlanComposer() {}
+    virtual ~JUNOScriptVlanComposer() {}
+    bool setVlan(uint32 vlanId, bool isToDelete);
+    
+private:
+    static const char* jsTemplate;
+
+};
 #endif
 
