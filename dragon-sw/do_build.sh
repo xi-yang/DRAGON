5993c89d7866568d296ac9b9ffac1b9253984fb1
chris <chris@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-03-23 19:47:58 +0000

    do_build.sh will now check to see if a file called 'do_build.conf' exists.
    If it does, it expects this file to define several variables that controls
    the build process.  This allows the user to override the default build 
    behavior when just running './do_build.sh', specifying a certain build target
    to always be used, a certain CLI type, switch username/password, SNMP community
    string, etc.
    
    A file called 'do_build.conf.sample' included, to show what 'do_build.conf'
    should look like, and what each of the options mean.
    
    The intended usage is to copy 'do_build.conf.sample' to 'do_build.conf', then
    make local modifications to that file, such that every time the code is re-built,
    the same build options are applied.
    
    
    
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2207 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/do_build.sh
+++ dragon-sw/do_build.sh
@@ -1,59 +1,77 @@
 #!/bin/sh
 #
 
-SNMP_COMMUNITY=dragon
+CONF_FILE="do_build.conf"
 PREFIX=/usr/local/dragon
 
-target=$1
+if test -f $CONF_FILE; then
+    . $CONF_FILE
+fi
 
-if test "$target" = ""; then
-  target=default
+if test "x$1" != x; then
+  target=$1
+fi
+
+if test "x$target" = x; then
+  target="default"
+fi
+
+if test "x$snmp_community" = x; then
+  snmp_community="dragon"
+fi
+
+if test "x$switch_ctrl_port" = x; then
+  switch_ctrl_port="255"
+fi
+
+if test "x$debug_symbols" = x; then
+  debug_symbols="yes"
 fi
 
 echo "Building softare target: $target ..."
 
-rsvpconf=
-zebraconf="--disable-bgpd --disable-ripd --disable-ripngd --disable-ospf6d"
+rsvpconf="--prefix=$PREFIX --disable-java-api --disable-altq"
+zebraconf="--prefix=$PREFIX --enable-dragon --disable-bgpd --disable-ripd --disable-ripngd --disable-ospf6d"
 
 case "$target" in
   vlsr|VLSR|csa|CSA|narb|NARB|default)
-    rsvpconf="--with-switch-vendor-model=AutoDetect --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY"
+    rsvpconf="$rsvpconf --with-switch-vendor-model=AutoDetect --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community"
   ;;
   vlsr-force10-v5|VLSR-FORCE10-V5)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=Force10E600 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=Force10E600 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-force10-v6|VLSR-FORCE10-V6)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=Force10E600 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --with-force10-software-v6 --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=Force10E600 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --with-force10-software-v6 --enable-switch-port-shutdown"
   ;;
   vlsr-juniper|VLSR-JUNIPER)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=JuniperEX3200 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=JuniperEX3200 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-raptor|VLSR-RAPTOR)
-    rsvpconf="--with-switch-vendor-model=RaptorER1010 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY"
+    rsvpconf="$rsvpconf --with-switch-vendor-model=RaptorER1010 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community"
   ;;
   vlsr-raptor-qos|VLSR-RAPTOR-QOS)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=RaptorER1010 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=RaptorER1010 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-cat3750|vlsr-catalyst3750|VLSR-CATALYST3750|VLSR-Catalyst3750)
-    rsvpconf="--with-switch-vendor-model=Catalyst3750 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY"
+    rsvpconf="$rsvpconf --with-switch-vendor-model=Catalyst3750 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community"
   ;;
   vlsr-cat3750-qos|vlsr-catalyst3750-qos|VLSR-CATALYST3750-QOS|VLSR-Catalyst3750-QoS)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=Catalyst3750 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=Catalyst3750 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-cat6500|vlsr-catalyst6500|VLSR-CATALYST6500|VLSR-Catalyst6500)
-    rsvpconf="--with-switch-vendor-model=Catalyst6500 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --with-switch-vendor-model=Catalyst6500 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-cat6500-qos|vlsr-catalyst6500-qos|VLSR-CATALYST6500-QOS|VLSR-Catalyst6500-QoS)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=Catalyst6500 --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY --enable-switch-port-shutdown"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=Catalyst6500 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
   vlsr-subnet|VLSR-SUBNET)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=AutoDetect --with-switch-ctrl-port=255 --with-switch-snmp-community=$SNMP_COMMUNITY"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=AutoDetect --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community"
   ;;
   vlsr-linux|VLSR-LINUX)
-    rsvpconf="--enable-switch-cli-access --with-switch-vendor-model=LinuxSwitch --with-switch-ctrl-port=1 --with-switch-snmp-community=$SNMP_COMMUNITY"
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=LinuxSwitch --with-switch-ctrl-port=1 --with-switch-snmp-community=$snmp_community"
   ;;
   vlsr-verbose|VLSR-VERBOSE|vlsr-interactive|VLSR-INTERACTIVE)
-    rsvpconf="--enable-switch-cli-access"
+    rsvpconf="$rsvpconf --enable-switch-cli-access"
   ;;
   cleanup)
     cd zebra
@@ -82,6 +100,23 @@ case "$target" in
   ;;
 esac
 
+if test "x$debug_symbols" = "xyes"; then
+  rsvpconf="$rsvpconf CFLAGS=-g CPPFLAGS=-g"
+  zebraconf="$zebraconf CFLAGS=-g CPPFLAGS=-g"
+fi
+
+if test "x$switch_cli_type" != x; then
+  rsvpconf="$rsvpconf --with-switch-cli-type=$switch_cli_type"
+fi
+
+if test "x$switch_cli_username" != x; then
+  rsvpconf="$rsvpconf --with-switch-cli-username=$switch_cli_username"
+fi
+
+if test "x$switch_cli_password" != x; then
+  rsvpconf="$rsvpconf --with-switch-cli-password=$switch_cli_password"
+fi
+
 case `uname` in
   *BSD)
     echo This is BSD Unix
@@ -92,7 +127,7 @@ case `uname` in
     echo '' && \
 	echo 'configuring kom-rsvp...'
     cd kom-rsvp
-    $CONFIG_SHELL ./configure --prefix=$PREFIX --disable-java-api --disable-altq $rsvpconf CFLAGS=-g CPPFLAGS=-g
+    $CONFIG_SHELL ./configure $rsvpconf
     if test $? != 0; then
 	echo "dragon-sw: kom-rsvp configure error!"
 	exit 1
@@ -110,7 +145,7 @@ case `uname` in
     echo '' && \
 	echo 'configuring zebra...'
     cd ../zebra
-    $CONFIG_SHELL ./configure --prefix=$PREFIX --enable-dragon $zebraconf CFLAGS=-g CPPFLAGS=-g
+    $CONFIG_SHELL ./configure $zebraconf
     if test $? != 0; then
 	echo "dragon-sw: zebra configure error!"
 	exit 1
@@ -136,7 +171,7 @@ case `uname` in
     echo '' && \
 	echo 'configuring kom-rsvp...'
     cd kom-rsvp
-    ./configure --prefix=$PREFIX --disable-java-api --disable-altq $rsvpconf CFLAGS=-g CPPFLAGS=-g
+    ./configure $rsvpconf
     if test $? != 0; then
 	echo "dragon-sw: kom-rsvp configure error!"
 	exit 1
@@ -160,7 +195,7 @@ case `uname` in
     echo '' && \
 	echo 'configuring zebra...'
     cd ../zebra
-    ./configure --prefix=$PREFIX --enable-dragon $zebraconf CFLAGS=-g CPPFLAGS=-g
+    ./configure $zebraconf
     if test $? != 0; then
         echo "dragon-sw: zebra configure error!"
         exit 1
@@ -186,7 +221,7 @@ case `uname` in
     echo '' && \
         echo 'configuring kom-rsvp...'
     cd kom-rsvp
-    ./configure --prefix=$PREFIX --disable-java-api --disable-altq --disable-cbq --disable-traffgen $rsvpconf CFLAGS=-g CPPFLAGS=-g
+    ./configure --disable-cbq --disable-traffgen $rsvpconf
     if test $? != 0; then
         echo "dragon-sw: kom-rsvp configure error!"
         exit 1
@@ -204,7 +239,7 @@ case `uname` in
     echo '' && \
         echo 'configuring zebra...'
     cd ../zebra
-    ./configure --prefix=$PREFIX --enable-dragon $zebraconf CFLAGS=-g CPPFLAGS=-g
+    ./configure $zebraconf
     if test $? != 0; then
         echo "dragon-sw: zebra configure error!"
         exit 1
