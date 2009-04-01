dc238db290fe3aecc085b0ffcf784c0c45e43d8d
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-04-01 21:36:02 +0000

    do_build support for dell6024-qos. (6224 and 6248 are pending on finishing touch when we have access to a real switch)
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2223 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/do_build.sh
+++ dragon-sw/do_build.sh
@@ -52,6 +52,9 @@ case "$target" in
   vlsr-raptor-qos|VLSR-RAPTOR-QOS)
     rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=RaptorER1010 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
   ;;
+  vlsr-dell6024-qos|VLSR-DELL6024-QOS)
+    rsvpconf="$rsvpconf --enable-switch-cli-access --with-switch-vendor-model=PowerConnect6024 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community --enable-switch-port-shutdown"
+  ;;
   vlsr-cat3750|vlsr-catalyst3750|VLSR-CATALYST3750|VLSR-Catalyst3750)
     rsvpconf="$rsvpconf --with-switch-vendor-model=Catalyst3750 --with-switch-ctrl-port=$switch_ctrl_port --with-switch-snmp-community=$snmp_community"
   ;;
