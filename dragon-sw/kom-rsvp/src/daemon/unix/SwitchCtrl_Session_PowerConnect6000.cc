f28f05eec8c95ed7a113b00fd1ed531fe0cf4e8b
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-04-02 18:46:59 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2241 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_PowerConnect6000.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_PowerConnect6000.cc
@@ -137,9 +137,9 @@ bool SwitchCtrl_Session_PowerConnect6000_CLI::policeInputBandwidth(bool do_undo,
         DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, "Match access-group", true, 1, 10)) ;
         if (n == 1) // the class-map (and corresponding policy-map) have already been removed
         {
-            DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
             return postAction();
         }
+        DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
         DIE_IF_NEGATIVE(n= writeShell( "configure\n", 5)) ;
         DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
         // remove vlan-level policy map
