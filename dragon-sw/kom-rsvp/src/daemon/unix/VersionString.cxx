bf1c67e6b4459aa81d088d67148ea6bcb88c42bb
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2009-07-08 22:06:50 +0000

    version mark: support for non-contiguous subnet edge interfaces
    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2313 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/VersionString.cxx
+++ dragon-sw/kom-rsvp/src/daemon/unix/VersionString.cxx
@@ -6,7 +6,7 @@ const char* VersionString() {
 	return "Release 3.1pre - " BUILD_DATE;
 }
 
-/*** To automate version string generation from SVN, CHANGE HERE into ANYTHING before each 'commit' --> "06/03/2009" ***/
+/*** To automate version string generation from SVN, CHANGE HERE into ANYTHING before each 'commit' --> "07/08/2009" ***/
 
 const char* DragonVersionString() {
     static char dvString[100];
