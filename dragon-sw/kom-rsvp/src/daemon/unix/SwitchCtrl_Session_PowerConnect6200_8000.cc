6e93f537eb48f44cb4424f5bf8fc92da7a05dcf2
xi <xi@625cd551-afe4-0310-9ad4-9d199310cf85>
2011-03-10 22:53:46 +0000

    git-svn-id: file:///var/www/svn/dragon-sw/trunk@2428 625cd551-afe4-0310-9ad4-9d199310cf85

--- dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_PowerConnect6200_8000.cc
+++ dragon-sw/kom-rsvp/src/daemon/unix/SwitchCtrl_Session_PowerConnect6200_8000.cc
@@ -34,7 +34,7 @@ bool SwitchCtrl_Session_PowerConnect8000::preAction()
     if (n == 1)
     {
         DIE_IF_NEGATIVE(n= writeShell( "enable\n", 5)) ;
-        DIE_IF_NEGATIVE(n= readShell( "Password: ", NULL, 0, 10)) ;
+        DIE_IF_NEGATIVE(n= readShell( "Password:", NULL, 0, 10)) ;
         if (strcmp(CLI_PASSWORD, "unknown") != 0)
             DIE_IF_NEGATIVE(n= writeShell( CLI_PASSWORD, 5)) ;
         DIE_IF_NEGATIVE(n= writeShell( "\n", 5)) ;
@@ -186,6 +186,7 @@ bool SwitchCtrl_Session_PowerConnect8000::policeInputBandwidth(bool do_undo, uin
         DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
         DIE_IF_NEGATIVE(n= writeShell( "match vlan ", 5)) ;
         DIE_IF_NEGATIVE(n= writeShell( vlanNum, 5)) ;
+        DIE_IF_NEGATIVE(n= writeShell( "\n", 5)) ;
         DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
         DIE_IF_NEGATIVE(n= writeShell( "exit\n", 5)) ;
         DIE_IF_NEGATIVE(n= readShell( SWITCH_PROMPT, NULL, 1, 10)) ;
